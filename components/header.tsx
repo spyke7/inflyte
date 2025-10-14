2640895bb3466bf5d7d4c36d5335281dcd3009b8
spyke7 <dolaishreejan@gmail.com>
2025-10-14 21:03:05 +0530

    Change of name

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,5 @@
 'use client'
 import Link from 'next/link'
-import { Logo } from '@/components/logo'
 import { Menu, X } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import React from 'react'
@@ -34,7 +33,7 @@ export const HeroHeader = () => {
                                 href="/"
                                 aria-label="home"
                                 className="flex items-center space-x-2 text-2xl font-semibold tracking-tight">
-                                InFlow<span className="text-primary">.</span>
+                                Inflyte<span className="text-primary">.</span>
                             </Link>
 
                             <button
--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -24,7 +24,7 @@ export default function HeroSection() {
               href="#link"
               className="hover:bg-background dark:hover:border-t-border bg-muted group mx-auto flex w-fit items-center gap-4 rounded-full border p-1 pl-4 shadow-md shadow-zinc-950/5 transition-colors duration-300 dark:border-t-white/5 dark:shadow-zinc-950"
             >
-              <span className="text-foreground text-sm">Introducing InFlow API Suite</span>
+              <span className="text-foreground text-sm">Introducing Inflyte API Suite</span>
               <span className="dark:border-background block h-4 w-0.5 border-l bg-white dark:bg-zinc-700"></span>
               <div className="bg-background group-hover:bg-muted size-6 overflow-hidden rounded-full duration-500">
                 <div className="flex w-12 -translate-x-1/2 duration-500 ease-in-out group-hover:translate-x-0">
@@ -52,7 +52,7 @@ export default function HeroSection() {
             as="p"
             className="mx-auto mt-8 max-w-2xl text-balance text-lg text-muted-foreground"
           >
-            InFlow lets you analyze, generate charts, understand as well as build APIs in minutes — create, visualize requests, and monitor everything in one dashboard.
+            Inflyte lets you analyze, generate charts, understand as well as build APIs in minutes — create, visualize requests, and monitor everything in one dashboard.
           </TextEffect>
 
           <AnimatedGroup
